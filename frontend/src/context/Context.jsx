388eaae81f7dcb5bbec3be3e4fdf77970b89f6d8
Aidan <aidanjosiah02@gmail.com>
2025-10-04 16:58:13 -0700

    Co-authored-by: Jason <jasonandkhangminh@gmail.com> Co-authored-by: Jumpshawt <Jumpshawt@users.noreply.github.com>

--- frontend/src/context/Context.jsx
+++ frontend/src/context/Context.jsx
@@ -4,10 +4,7 @@ import { MarkersContext, MarkersContextUpdate } from './ContextHook';
 
 
 export function Context({ children }) {
-    const [markers, setMarkers] = useState([
-        { lat: 60.87, lng: 52, name: "somename", description: "some description", map: "some map", collection: "somecollection", zoom: 2 },
-        { lat: 52, lng: 21.76, name: "somename2", description: "description2", map: "map", collection: "somecollection", zoom: 2 }
-    ])
+    const [markers, setMarkers] = useState([])
     return (
         <MarkersContext.Provider value={markers}>
             <MarkersContextUpdate.Provider value={setMarkers}>
