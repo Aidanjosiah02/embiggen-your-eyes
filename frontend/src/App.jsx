7b6928535f43b2a89a5583b90bfc94776197e174
Alerius228 <KlimovAndrey228@gmail.com>
2025-10-05 13:13:27 -0700

    feat: build and frontend on the server side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,32 @@
-import { useState } from 'react'
-import Leaflet from './components/app/Leaflet.jsx'
-import SearchMarkers from './components/app/SearchMarkers.jsx'
-import { Context } from './context/Context'
+import { useState } from "react";
+import Leaflet from "./components/app/Leaflet.jsx";
+import SearchMarkers from "./components/app/SearchMarkers.jsx";
+import { Context } from "./context/Context";
 // import { useMarkers, useMarkerUpdate } from './context/ContextHook'
 // import sendMarkers from './components/sendMarkers.js'
-import Header from './components/app/Header.jsx'
-import SubmitMarkers from './components/app/SubmitMarkers.jsx'
-import SearchCollections from './components/app/SearchCollections.jsx'
+import Header from "./components/app/Header.jsx";
+import SubmitMarkers from "./components/app/SubmitMarkers.jsx";
+import SearchCollections from "./components/app/SearchCollections.jsx";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   // const markerCollection = useMarkerCollection();
 
   return (
     <>
       <Context>
         <Header />
-        <main className='main'>
+        <main className="main">
           <Leaflet />
           <SearchMarkers />
           <SubmitMarkers />
           <SearchCollections />
         </main>
+        <LeafletGIBS />
       </Context>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
